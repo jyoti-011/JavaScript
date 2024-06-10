8b3b36266cdc393ea5e2a1c78acd9080a3114bc0
Jyoti Pal <jyotipal4112003@gmail.com>
2024-06-11 00:10:44 +0530

    summary of datatypes

---
+++ 01_Basics/datatype-summary.js
@@ -0,0 +1,5 @@
+// Primitive Datatype (Call by Value)
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// Reference (Non-Primitive) (Call by reference)
+//Arrays, Objects, Functions
