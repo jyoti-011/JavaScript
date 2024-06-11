13082bb3304396df42668dea3ab28483d5f5ded1
Jyoti Pal <jyotipal4112003@gmail.com>
2024-06-11 11:36:56 +0530

    memory management of datatypes

--- 01_Basics/datatype-summary.js
+++ 01_Basics/datatype-summary.js
@@ -33,13 +33,39 @@ const myfunction = function(){
 
 
 //To find out the type of variable use typeof
-console.log(typeof score);   //number
-console.log(typeof value);   //number
-console.log(typeof isLoggedIn); //boolean
-console.log(typeof outsideTemp);//object
-console.log(typeof userEmail); //undefined
-console.log(typeof id);//symbol
-console.log(typeof bigNumber);//bigint
-console.log(typeof arr);//object
-console.log(typeof myobj);//object
-console.log(typeof myfunction);//function
+// console.log(typeof score);   //number
+// console.log(typeof value);   //number
+// console.log(typeof isLoggedIn); //boolean
+// console.log(typeof outsideTemp);//object
+// console.log(typeof userEmail); //undefined
+// console.log(typeof id);//symbol
+// console.log(typeof bigNumber);//bigint
+// console.log(typeof arr);//object
+// console.log(typeof myobj);//object
+// console.log(typeof myfunction);//function
+
+/* *********************************************************************** */
+
+//Memory Management
+/*
+There are two types of memory:
+1.Stack - It stores Primitive datatypes
+2.Heap - It stores Non-Primitive Datatypes*/
+
+//Example:
+let myYoutubeChannelname="Tesla"
+let anotherName=myYoutubeChannelname
+anotherName="Tessy"
+console.log(myYoutubeChannelname);
+console.log(anotherName);
+//In stack the copy of value is assigned therefore original value is not changed
+
+let userOne={
+    email:"user@google.com",
+    upi:"user@ybl"
+}
+let userTwo=userOne
+userTwo.email="abc@google.com"
+console.log(userOne.email);
+console.log(userTwo.email);
+//In heap the reference of the value is assigned therefore original value is also changed
